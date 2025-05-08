ce3d05b8d7ce7236f717d185f35e0928eba1dbde
ShaheerJamalChishti09 <shaheerjamal369@gmail.com>
2025-05-08 22:59:28 +0500

    Learning Python 2025

--- Chapter 07/For Loop Practice Questions/03.py
+++ Chapter 07/For Loop Practice Questions/03.py
@@ -1,11 +1,11 @@
 # Factorial: Write a program that calculates the factorial of a number using a for loop. For example, factorial of 5 is 5 × 4 × 3 × 2 × 1.
 
-n = int(input("number here: "))
+# n = int(input("number here: "))
 
-for i in range(1, n):
-    n*=i # 5 = 5 * 1
-    i += 1 # i = i + 1
-print(n)
+# for i in range(1, n):
+#     n*=i # 5 = 5 * 1
+#     i += 1 # i = i + 1
+# print(n)
 
 '''
 
@@ -29,4 +29,39 @@ n = 30 x 4 => 120
 i = 4 + 1 => 5
 
 
+'''
+
+num = int(input("Enter a number: "))
+factorial = 1
+
+if num < 0:
+    print("Factorial is not defined for negative numbers.")
+else:
+    for i in range(1, num + 1):
+        factorial *= i # factorial = factorial * i
+    print("The factorial of", num, "is", factorial)
+
+
+'''
+num = 5
+factorial = 1
+
+1 -- 5
+
+#This is the first iteration of the loop
+1 = 1 x 1 => 1 #Now the new value of factorial is 1
+
+#Tghis is the second iteration of the loop
+2 = 1 x 2 => 2 #Now the new value of factorial is 2
+
+#This is the third iteration of the loop
+3 = 2 x 3 => 6 #Now the new value of factorial is 6
+
+#This is the fourth iteration of the loop
+4 = 6 x 4 => 24 #Now the new value of factorial is 24
+
+#This is the fifth iteration of the loop
+5 = 24 x 5 => 120 #Now the new value of factorial is 120
+
+The factorial of num is: 120
 '''

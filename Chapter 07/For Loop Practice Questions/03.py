b1f55b0d15fa30fcd3c3ae39d31a5bab5b6802d8
ShaheerJamalChishti09 <shaheerjamal369@gmail.com>
2025-05-08 23:07:04 +0500

    Learning Python 2025

--- Chapter 07/For Loop Practice Questions/03.py
+++ Chapter 07/For Loop Practice Questions/03.py
@@ -1,14 +1,13 @@
 # Factorial: Write a program that calculates the factorial of a number using a for loop. For example, factorial of 5 is 5 × 4 × 3 × 2 × 1.
 
-# n = int(input("number here: "))
+n = int(input("number here: "))
 
-# for i in range(1, n):
-#     n*=i # 5 = 5 * 1
-#     i += 1 # i = i + 1
-# print(n)
+for i in range(1, n):
+    n*=i # 5 = 5 * 1 / n = n * i
+    i += 1 # i = i + 1 / i = 1 + 1
+print(n)
 
 '''
-
 OUTPUT:
 number: 5
 1-5
@@ -16,30 +15,34 @@ number: 5
 n = n * i
 i = i + 1
 
-5 = 5 x 1 => 5
-i = 1 + 1 => 2
+This is the first iteration of the loop
+5 = 5 x 1 => 5 The value of factorial(n) is now updated and is now 5
+i = 1 + 1 => 2 Similarly, the value of i is updated to 2
 
-5 = 5 x 2 => 10
-i = 2 + 1 => 3
+This is the second iteration of the loop
+5 = 5 x 2 => 10 Now the new value of factorial is 10
+i = 2 + 1 => 3 Similarly, the value of i is updated to 3
 
-10 = 10 x 3 => 30 
-i = 3 + 1 => 4
+This is the third iteration of the loop
+10 = 10 x 3 => 30 This time the value of factorial is 30
+i = 3 + 1 => 4 and the value of i is updated to 4
 
-n = 30 x 4 => 120
-i = 4 + 1 => 5
+This is the fourth and the last iteration of the loop
+n = 30 x 4 => 120  #Now the new value of factorial is 120
+i = 4 + 1 => 5 and the value of i is updated to 5
 
-
+The factorial of num is: 120
 '''
 
-num = int(input("Enter a number: "))
-factorial = 1
+# num = int(input("Enter a number: "))
+# factorial = 1
 
-if num < 0:
-    print("Factorial is not defined for negative numbers.")
-else:
-    for i in range(1, num + 1):
-        factorial *= i # factorial = factorial * i
-    print("The factorial of", num, "is", factorial)
+# if num < 0:
+#     print("Factorial is not defined for negative numbers.")
+# else:
+#     for i in range(1, num + 1):
+#         factorial *= i # factorial = factorial * i
+#     print("The factorial of", num, "is", factorial)
 
 
 '''

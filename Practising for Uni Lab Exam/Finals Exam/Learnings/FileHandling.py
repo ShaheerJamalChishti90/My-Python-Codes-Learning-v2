c7444d69bcbd668c7988e6adbd9c3a8f5676b0f9
ShaheerJamalChishti09 <shaheerjamal369@gmail.com>
2025-07-19 00:37:28 +0500

    Shaheer Python Learnings 2025

---
+++ Practising for Uni Lab Exam/Finals Exam/Learnings/FileHandling.py
@@ -0,0 +1,7 @@
+# f = open("shaheer.txt", "w")
+# f.write("Hello, Shaheer!")
+# f.close()
+
+with open("./Learnings/shaheer v3.txt", "w") as f:
+    f.write("Shaheer! This is the version 3 of creating a file inside a specific directory")
+    

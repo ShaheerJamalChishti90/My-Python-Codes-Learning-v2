2c7dcfe8ea2f91e8bcd627d7a736b547711c1790
ShaheerJamalChishti09 <shaheerjamal369@gmail.com>
2025-07-19 00:15:28 +0500

    Shaheer Python Learings 2025

---
+++ Practising for Uni Lab Exam/Practicing FlowCharts/main.py
@@ -0,0 +1,10 @@
+a  = 1.5 
+b  = 5
+c  = 2
+
+if a > b and a > c:
+    print("a is greater than b and c")
+elif b > c and b > a:
+    print("b is greater than c and a")
+else:
+    print("c is the greatest")

eebda5391c8ac562bdefe64c9db67b916a2ed804
ShaheerJamalChishti90 <shaheerjamal369@gmail.com>
2025-07-20 23:19:17 +0500

    Shaheer Python Codes

--- Python 10  Important Functions/Enmurate.py
+++ Python 10  Important Functions/Enmurate.py
@@ -1,12 +1,29 @@
 tasks = ["write code", "attend meeting", "write report", "off"]
 
-for i in range(len(tasks)):
-    task = tasks[i]
-    print(f"{i+1}: {task}")
+# for i in range(len(tasks)):
+#     task = tasks[i]
+#     print(f"{i+1}: {task}")
     
     
     
-for i, task in enumerate(tasks):
-    print(f"{i+1}: {task}")
+# for i, task in enumerate(tasks):
+#     print(f"{i+1}: {task}")
 
-print(list(enumerate(tasks)))
+# print(list(enumerate(tasks)))
+
+marks = [48, 58, 78, 72, 85]
+
+# index = 0
+# for i in marks:
+#     print(i)
+#     if index == 3:
+#         print("Shaheer scored this marks")
+#     index += 1
+    
+    
+for shaheer, i in enumerate(marks):
+    print(i)
+    if shaheer == 3:
+        print("This guy shaheer scored this marks")
+
+    
